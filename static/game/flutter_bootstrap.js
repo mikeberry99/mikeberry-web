f72cb3b3fc6019cc53ad9f90b024c7f5607eec16
Mike Berry <email@mikeberry.dev>
2026-07-15 17:52:52 +1000

    Fixed custom icon references

--- static/game/flutter_bootstrap.js
+++ static/game/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1647980675" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3445043571" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
